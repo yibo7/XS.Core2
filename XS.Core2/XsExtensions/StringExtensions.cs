e72253a3bb8b720846b94fa926eadbfbd4f39b3a
yibo7 <cqs263@163.com>
2023-02-20 21:55:44 +0800

    string

--- XS.Core2/XsExtensions/StringExtensions.cs
+++ XS.Core2/XsExtensions/StringExtensions.cs
@@ -6,11 +6,101 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using XS.Core2.Strings;
+using System.Web;
+using System.Net;
+using XS.Core2.Encrypts;
 
 namespace XS.Core2.XsExtensions
 {
     public static class StringExtensions
     {
+        public static string ToBase64(this string strSoure)
+        {
+            byte[] bytesToEncode = Encoding.UTF8.GetBytes(strSoure); // 将字符串转换为byte数组
+            return  Convert.ToBase64String(bytesToEncode);
+        }
+
+        public static string AESDecode(this string strSoure, string decryptKey)
+        {
+            return AES.Decode(strSoure, decryptKey);
+        }
+        public static string AESEncode(this string strSoure, string decryptKey)
+        {
+            return AES.Encode(strSoure, decryptKey);
+        }
+
+        public static string SHA256(this string strSoure)
+        {
+            return Md5Helper.SHA256(strSoure);
+        }
+        public static string Md5(this string strSoure)
+        {
+            return Md5Helper.MD5(strSoure);
+        }
+        /// <summary>
+        /// 通过换行符号将字符串分割成数组
+        /// </summary>
+        /// <param name="strSoure"></param>
+        /// <returns></returns>
+        public static string[] SplitByWrap(this string strSoure)
+        {
+            return GetString.GetArrByWrap(strSoure);
+        }
+
+        /// <summary>
+        /// 用指定字符标记分割成数组
+        /// </summary>
+        /// <param name="strSoure"></param>
+        /// <param name="strSplit">指定标记</param>
+        /// <returns></returns>
+        public static string[] SplitByTag(this string strSoure, string strSplit)
+        {
+            return GetString.SplitString(strSoure, strSplit);
+        }
+        /// <summary>
+        /// 返回 URL 字符串的编码结果
+        /// </summary>
+        /// <param name="str">字符串</param>
+        /// <returns>编码结果</returns>
+        public static string UrlEncode(this string strSoure)
+        {
+            return WebUtility.UrlEncode(strSoure);
+        }
+        /// <summary>
+        /// 从开始与结束标签截取字符
+        /// </summary>
+        /// <param name="strSoure"></param>
+        /// <param name="start">开始标记</param>
+        /// <param name="end">结束标记</param>
+        /// <returns></returns>
+        public static string CutStr(this string strSoure, string start, string end)
+        {
+            Regex rg = new Regex("(?<=(" + start + "))[.\\s\\S]*?(?=(" + end + "))", RegexOptions.Multiline | RegexOptions.Singleline);
+            return rg.Match(strSoure).Value;
+        }
+        /// <summary>
+        /// 截取过长的字符
+        /// </summary>
+        /// <param name="strSoure"></param>
+        /// <param name="length">要截取的长度</param> 
+        /// <returns></returns>
+        public static string CutStrLen(this string strSoure, int length)
+        {
+            return GetString.GetSubString(strSoure, length,"...");
+        }
+        /// <summary>
+        /// 截取过长的字符
+        /// </summary>
+        /// <param name="strSoure"></param>
+        /// <param name="length">要截取的长度</param>
+        /// <param name="p_TailString">结尾符</param>
+        /// <returns></returns>
+        public static string CutStrLen(this string strSoure, int length, string p_TailString)
+        {
+            return GetString.GetSubString(strSoure, length, p_TailString);
+        }
+
         public static dynamic ToJsonDynamic(this string str)
         {
             // 定义一个正则表达式
